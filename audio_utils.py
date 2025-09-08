9231a611d3801d935b9dc155c09646c4bf8d3e0f
Aditya Vashist <113428158+Aditya8215@users.noreply.github.com>
2025-09-08 13:59:28 +0530

    Update audio_utils.py

--- audio_utils.py
+++ audio_utils.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 from pydub import AudioSegment
 
-def add_noise_to_audio(input_path, output_path, noise_level=0.005):
+def add_noise_to_audio(input_path, output_path, noise_level=0.05):
     try:
         sound = AudioSegment.from_wav(input_path)
         samples = np.array(sound.get_array_of_samples())
@@ -14,4 +14,5 @@ def add_noise_to_audio(input_path, output_path, noise_level=0.005):
         return True
     except Exception as e:
         st.error(f"Error adding noise: {e}")
+
         return False
